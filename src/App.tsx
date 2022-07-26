f946e35d64753e1be35d60d29239348e21cc8c7e
Rohil Saraf <rohilsaraf97@gmail.com>
2022-07-26 19:55:49 +0530

    build: setup extension with webpack

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1 className="heading">
+        This is another rgeveev ejer v u u y gr enjtest
+      </h1>
+    </div>
+  );
+};
+
+export default App;
